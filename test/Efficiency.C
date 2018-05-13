8d7ecd48e962ab02c973788951f74630d88c182b
Antonio Vagnerini <antonio.vagnerini@desy.de>
2018-05-13 18:33:20 +0200

    updating json file

--- test/Efficiency.C
+++ test/Efficiency.C
@@ -22,18 +22,24 @@
 #include "Fit/Fitter.h"
 #include "Fit/BinData.h"
 #include "Fit/Chi2FCN.h"
-#include "TH1.h"
+#include "Fit/PoissonLikelihoodFCN.h"
 #include "TList.h"
 #include "Math/WrappedMultiTF1.h"
 #include "HFitInterface.h"
-#include "TCanvas.h"
 #include "TStyle.h"
 
 
 using namespace std;
 
-static const double mJPsi = 3.10; //pdg value Y(1s) resonance
+static const double mJPsi = 3.0969; //pdg value Y(1s) resonance
 
+/* useful typedefs
+
+typedef Chi2FCN< ROOT::Math::IMultiGenFunction > 	ROOT::Fit::Chi2Function
+typedef PoissonLikelihoodFCN<ROOT::Math::IMultiGenFunction, ROOT::Math::IParamMultiFunction> PoissonLLFunction;
+typedef PoissonLikelihoodFCN<ROOT::Math::IMultiGradFunction, ROOT::Math::IParamMultiFunction> PoissonLLGradFunction;
+
+*/
 
 // =============================================================================================   
 
@@ -55,7 +61,7 @@ int iparU[5] = { 0,      // amplitude (gaus)
 		 4,
                };
 
-
+//Global Chi2
 struct GlobalChi2 {
   GlobalChi2(  ROOT::Math::IMultiGenFunction & f1,
 	       ROOT::Math::IMultiGenFunction & f2) :
@@ -77,15 +83,38 @@ struct GlobalChi2 {
   const  ROOT::Math::IMultiGenFunction * fChi2_2;
 };
 
+//Global Likelihood
+struct GlobalLikelihood {
+  GlobalLikelihood(  ROOT::Math::IMultiGenFunction & f1,
+		     ROOT::Math::IMultiGenFunction & f2) :
+    fLikelihood_1(&f1), fLikelihood_2(&f2) {}
+  
+  // parameter vectors
+  
+  double operator() (const double *par) const {
+    double p1[5];
+    for (int i = 0; i < 5; ++i) p1[i] = par[iparM[i] ];
+    
+    double p2[5];
+    for (int i = 0; i < 5; ++i) p2[i] = par[iparU[i] ];
+    
+    return (*fLikelihood_1)(p1) + (*fLikelihood_2)(p2);
+  }
+  
+  const  ROOT::Math::IMultiGenFunction * fLikelihood_1;
+  const  ROOT::Math::IMultiGenFunction * fLikelihood_2;
+};
+
 // =============================================================================================   
-void Efficiency( string hltPath_, string era_, double halfwidth_ )
+void Efficiency( string hltPath_, string era_, double halfwidth_, string estimator_ )
 {
-     
+   setTDRStyle();
+
    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
    std::map<std::string, TH1F*> h1;
 
    std::vector<std::pair<float,float> > pt_bins;
-   if ( hltPath_ == "HLT_Mu8_v" ) pt_bins = {{7.75,8.25},{8.25,9},{9,11},{11,15},{15,50}};
+   if ( hltPath_ == "HLT_Mu8_v" ) pt_bins = {{7.75,8.25},{8.25,9},{9,11},{11,30}};
    else if (hltPath_=="HLT_SingleJet30_Mu12_SinglePFJet40_v") pt_bins = {{11,13},{13,15},{15,50}};
 
 
@@ -95,7 +124,8 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
 
    for ( size_t i = 0 ; i < eras.size(); i++ ) 
      {
-       if      ( hltPath_ == "HLT_Mu8_v" )                        f[i]= new TFile( ("ROOTFILES/DOUBLEMUON/histograms_2017"+eras[i]+".root").c_str(),"OLD");
+       //       if      ( hltPath_ == "HLT_Mu8_v" )                        f[i]= new TFile( ("ROOTFILES/DOUBLEMUON/histograms_2017"+eras[i]+".root").c_str(),"OLD");
+       if      ( hltPath_ == "HLT_Mu8_v" )                        f[i]= new TFile( ("ROOTFILES/DOUBLEMUON/RERECO/histograms_2017"+eras[i]+".root").c_str(),"OLD");    
        else if (hltPath_=="HLT_SingleJet30_Mu12_SinglePFJet40_v") f[i]= new TFile( ("ROOTFILES/BTAGCSV/histograms_2017"+eras[i]+".root").c_str(),"OLD");
 
         
@@ -134,10 +164,6 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
 	   cout<< " ==============================================="<<endl;
 	   cout<< " pT bin " << (pt_bins[i].first+pt_bins[i].second)/2 << endl;
 
-	   //histos 
-
-	   //	   TH1D * hM = new TH1D("hM","histo B",100,0,100);
-	   //	   TH1D * hU = new TH1D("hU","histo S+B",100, 0,100);
 	   string hps_ptbin = Form("m12_mutagnprobe_%.2fto%.2f"   , pt_bins[i].first, pt_bins[i].second);
            string hfl_ptbin = Form("m12_mutagnprobefl_%.2fto%.2f" , pt_bins[i].first, pt_bins[i].second);
 
@@ -146,27 +172,14 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
 	   TF1 * gausU = new TF1("gausU","(1-[3])*(gaus(0) + [4])", mJPsi -hw , mJPsi +hw );
 
 	   gausM->SetParameters( 10., mJPsi , 0.5*hw , 0.5 , -1.); //-0.1
-	   /*    gausM->SetParLimits(0, 0.01, 1000. );
-           gausM->SetParLimits(1, mJPsi - hw , mJPsi + hw );
-           gausM->SetParLimits(2, 0.2*hw, 2*hw );
-           gausM->SetParLimits(3, 0.1, 1.0 ); 
-           gausM->SetParLimits(4, -20., 20.);
-	   */
            gausU->SetParameters( 10., mJPsi , 0.5*hw , 0.5 , -1.);
-	   /*  gausU->SetParLimits(0, 0.01, 1000. );
-           gausU->SetParLimits(1, mJPsi - hw , mJPsi + hw );
-           gausU->SetParLimits(2, 0.2*hw, 2*hw );
-           gausU->SetParLimits(3, 0.1, 1.0 );
-           gausU->SetParLimits(5, -20., 20.);
-	   */
 
-	   //Fit
+	   //Fit options
 	   ROOT::Math::WrappedMultiTF1 wgausM (*gausM,1);
 	   ROOT::Math::WrappedMultiTF1 wgausU (*gausU,1);
 
 	   ROOT::Fit::DataOptions opt;
 	   ROOT::Fit::DataRange rangeM;
-	   // set the data range
 	   rangeM.SetRange( mJPsi -hw , mJPsi +hw );
 	   ROOT::Fit::BinData dataM(opt,rangeM);
 	   ROOT::Fit::FillData(dataM, h1[hps_ptbin]);
@@ -176,20 +189,25 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
 	   ROOT::Fit::BinData dataU(opt,rangeU);
 	   ROOT::Fit::FillData(dataU, h1[hfl_ptbin]);
 
+	   //Chi2 method
 	   ROOT::Fit::Chi2Function chi2_M (dataM, wgausM);
 	   ROOT::Fit::Chi2Function chi2_U (dataU, wgausU);
-
 	   GlobalChi2 globalChi2(chi2_M, chi2_U);
-
+	  	   
+	   //Likelihood method
+	   ROOT::Fit::PoissonLLFunction Likelihood_M (dataM, wgausM);
+	   ROOT::Fit::PoissonLLFunction Likelihood_U (dataU, wgausU);
+	   GlobalLikelihood globalLikelihood(Likelihood_M, Likelihood_U) ;
+	   	   
 	   ROOT::Fit::Fitter fitter;
 	   const int Npar = 5; //6
-	   double par0[Npar] = { 10., mJPsi , 0.5*hw , 0.5 , -1. }; // add -0.1
+	   double par0[Npar] = { 10., mJPsi , 0.5*hw , 0.5 , 2. }; // add -0.1
 
-	   // create before the parameter settings in order to fix or set range on them
+	   // Parameter settings 
 	   fitter.Config().SetParamsSettings(5,par0); //6
 
-	   //	   fitter.Config().ParSettings(1).Fix(); // fix m0
-	   fitter.Config().ParSettings(1).SetLimits(mJPsi - hw , mJPsi + hw);
+	   fitter.Config().ParSettings(1).Fix(); // fix m0
+	   //fitter.Config().ParSettings(1).SetLimits(mJPsi - hw , mJPsi + hw);
 	   fitter.Config().ParSettings(2).SetLimits( 0.2*hw, 2*hw );
 	   fitter.Config().ParSettings(3).SetLimits( 0.1, 1.0 );
 	   fitter.Config().ParSettings(4).SetLimits( -20., 20. );
@@ -198,24 +216,21 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
 	   fitter.Config().MinimizerOptions().SetPrintLevel(0);
 	   fitter.Config().SetMinimizer("Minuit2","Migrad");
 
-	   // fit FCN function directly
+	   // Simultaneous Fit 
 	   // (specify optionally data size and flag to indicate that is a chi2 fit)
 
-	   fitter.FitFCN(Npar,globalChi2,0,dataM.Size()+dataU.Size(),true); //6 before
+	   if ( estimator_ == "Chi2" )      fitter.FitFCN(Npar,globalChi2      ,0,dataM.Size()+dataU.Size(),true);
+	   else if ( estimator_ == "LL" )   fitter.FitFCN(Npar,globalLikelihood,0,dataM.Size()+dataU.Size(),false); 
+	    
 	   ROOT::Fit::FitResult result = fitter.Result();
 	   result.Print(std::cout);
 
-	   //Trigger results no BKG subtraction
- 
-	   //cout << "trigeff= " <<  << " +/- " <<  << endl;
-
-	   setTDRStyle();
-
-	   TCanvas * c1 = new TCanvas("Simfit","Simultaneous fit of two histograms",
-                             10,10,700,700);
-	   c1->Divide(1,2);
+	   //PLOT SIM FIT
+	   
+	   TCanvas * c1 = new TCanvas("Simfit","Simultaneous fit of two histograms", 1400,1000);
+	   c1->Divide(2,1);
 	   c1->cd(1);
-	   gStyle->SetOptFit(1111);
+	   //	   gStyle->SetOptFit(1111);
 
 	   gausM->SetFitResult( result, iparM);
 	   gausM->SetRange(rangeM().first, rangeM().second);
@@ -232,7 +247,7 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
 	   h1[hfl_ptbin]->GetXaxis()->SetRangeUser( mJPsi - hw -0.1,  mJPsi + hw + 0.1);
 	   h1[hfl_ptbin]->Draw();
 
-	   c1->SaveAs(("PLOTS/"+hps_ptbin+era_+".png").c_str());
+	   c1->SaveAs(("PLOTS/"+hps_ptbin+era_+estimator_+".png").c_str());
 	   
 	   const std::vector<double> * pars = & result.Parameters();   
 	   const std::vector<double> * errs = & result.Errors();
@@ -244,19 +259,18 @@ void Efficiency( string hltPath_, string era_, double halfwidth_ )
        
      }
     //Plot trig eff
-   
-   setTDRStyle();
+ 
       
    //Plot trig eff 
-   TCanvas *c = new TCanvas("c","c");                                                                                                                                                                   
+   TCanvas *c = new TCanvas("c","c");    
+   h1["h_trig"]->GetXaxis()->SetRangeUser( 0.,30.);                                                                                                                          
    h1["h_trig"]->SetMaximum( 1.0 );                              
    h1["h_trig"]->SetTitle( hltPath_.c_str() );
-   h1["h_trig"]->GetXaxis()->SetTitle("pt_{probe} /GeV");                                                                                                                                                            
+   h1["h_trig"]->GetXaxis()->SetTitle("p_{Tprobe} [GeV]");                                                                                                                                                            
    h1["h_trig"]->GetYaxis()->SetTitle("trigger efficiency");
    h1["h_trig"]->Draw(); 
-   c->SaveAs(("PLOTS/"+hltPath_+era_+".png").c_str());  
-     
-     
+   c->SaveAs(("PLOTS/"+hltPath_+era_+estimator_+".png").c_str());  
+          
    
 }
 
